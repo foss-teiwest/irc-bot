363e91d5e12701be9001bcf62062fd0a93561082
Bill Kolokithas <kolokithas.b@gmail.com>
2013-07-23 05:24:34 +0300

    restart bot on failed exit

--- irc-bot.sh
+++ irc-bot.sh
@@ -5,8 +5,8 @@ nickpwd=xxxxxxxx
 echo $nickpwd | bin/irc-bot
 retval=$?
 
-while [ $retval -eq 142 ]; do
-	sleep 2
+while [ $retval -gt 0 ]; do
+	sleep 5
 	echo $nickpwd | bin/irc-bot
 	retval=$?
 done
