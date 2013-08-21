1b70d6e25325edace74a92f948f461fd983f54d0
Bill Kolokithas <kolokithas.b@gmail.com>
2013-08-21 20:39:12 +0300

    improve youtube script

--- scripts/youtube2mp3.sh
+++ scripts/youtube2mp3.sh
@@ -1,37 +1,52 @@
 #!/usr/bin/env bash
 
-TITLE=`youtube-dl --get-title $1`
+URL=`echo $1 | grep -o "^[^&]*"`
+TITLE=`youtube-dl --get-title --playlist-end 1 $URL`
 DIR=$2
 
-if [ -f "$DIR/$TITLE.mp3" ];
-then
+print_song() {
+
+	QUEUESIZE=`mpc playlist | wc -l`
+	echo "♪ $TITLE ♪ queued after `expr $QUEUESIZE - 1` song(s)..."
+}
+
+if [ -f "$DIR/$TITLE.mp3" ]; then
 	mpc add "$TITLE".mp3 && mpc -q play
-	echo "♪ $TITLE ♪ added in queue...";
-else
-	youtube-dl \
-	-q \
-	--skip-download \
-	--write-info-json \
-	-f 22/35/34 $1 \
-	-o "$DIR/$TITLE".mp3
-
-	DURATION=`cat "$DIR/$TITLE".mp3.info.json | json_reformat | grep duration | grep -o '[0-9]*'`
-
-	if [ "$DURATION" -gt "500" ];
-	then
-		echo "Error: The duration looks like my dick... too long!";
-		rm "$DIR/$TITLE".mp3;
+	if [ $? -eq 0 ]; then
+		print_song
 	else
-		youtube-dl \
-		-q \
-		--extract-audio \
-		--audio-format mp3 \
-		--audio-quality 192k \
-		-f 22/35/34 $1 \
-		-o "$DIR/$TITLE".mp3
+		echo "Could not play song"
+	fi
+else
+	youtube-dl            \
+	-q                    \
+	--skip-download       \
+	--playlist-end 1      \
+	--write-info-json     \
+	--max-quality 22 $URL \
+	-o "song"
+
+	DURATION=`cat song.info.json | json_reformat | grep duration | grep -o '[0-9]*'`
+	rm "song".info.json
+
+	if [ "$DURATION" -gt 480 ]; then
+		echo "Song longer than 8 mins, skipping..."
+	else
+		youtube-dl            \
+		-q                    \
+		--playlist-end 1      \
+		--max-filesize 100M   \
+		--extract-audio       \
+		--audio-format mp3    \
+		--audio-quality 192k  \
+		--max-quality 22 $URL \
+		-o "$DIR/$TITLE"."%(ext)s"
 
 		mpc -q update --wait && mpc add "$TITLE".mp3 && mpc -q play
-		echo "♪ $TITLE ♪ added in queue...";
+		if [ $? -eq 0 ]; then
+			print_song
+		else
+			echo "Could not play song"
+		fi
 	fi
-	rm "$DIR/$TITLE".mp3.info.json
 fi
