81d60d26f3148128e5b7e32b88747cea2db8cab0
Bill Kolokithas <kolokithas.b@gmail.com>
2014-03-07 03:04:05 +0200

    use socketpair for mocking instead of reading from files

--- test/test_check.h
+++ test/test_check.h
@@ -1,6 +1,15 @@
 #ifndef CHECK_CHECK_H
 #define CHECK_CHECK_H
 
+#include <check.h>
+
+#define READ  0
+#define WRITE 1
+
+void socketpair_open(void);
+void socketpair_close(void);
+void mock_write(int fd[2], const void *buffer, size_t len);
 Suite *socket_suite(void);
 
 #endif
+
--- test/test_socket.c
+++ test/test_socket.c
@@ -1,25 +1,59 @@
-#include <unistd.h>
 #include <check.h>
+#include <string.h>
+#include <unistd.h>
 #include "socket.h"
 #include "irc.h"
 #include "test_check.h"
 
-static Irc server;
-static int sock;
+ssize_t sock_readbyte(int sock, char *byte);
+
+extern int fd[2];
+char test_buffer[IRCLEN + 1];
+
 
 START_TEST(socket_connection) {
 
-	sock = sock_connect("www.google.com", "80");
+	int sock = sock_connect("www.google.com", "80");
 	ck_assert_msg(sock >= 0, "Socket creation failed");
 	close(sock);
 
 } END_TEST
 
+
 START_TEST(irc_connection) {
 
-	server = irc_connect("www.google.com", "80");
+	Irc server = irc_connect("www.google.com", "80");
 	ck_assert_msg(server != NULL, "irc connection failed");
-	quit_server(server, "bye");
+
+} END_TEST
+
+
+START_TEST(socket_readbytes) {
+
+	int i;
+	char c;
+	const char *msg = "lol\r\ntroll\r\n";
+
+	mock_write(fd, msg, strlen(msg));
+	for (i = 0; sock_readbyte(fd[READ], &c) > 0; i++)
+		test_buffer[i] = c;
+
+	test_buffer[i] = '\0';
+	ck_assert_str_eq(msg, test_buffer);
+
+} END_TEST
+
+
+START_TEST(socket_readline) {
+
+	char buffer[IRCLEN + 1] = { 0 };
+	const char *msg = "lol\r\ntroll\r\n";
+
+	mock_write(fd, msg, strlen(msg));
+	while (sock_readline(fd[READ], buffer, IRCLEN) > 0)
+		strcat(test_buffer, buffer);
+
+	ck_assert_str_eq("loltroll", test_buffer);
 
 } END_TEST
 
@@ -28,10 +62,16 @@ Suite *socket_suite(void) {
 
 	Suite *suite = suite_create("sockets");
 	TCase *core  = tcase_create("core");
+	TCase *sread = tcase_create("socket read");
 
 	suite_add_tcase(suite, core);
 	tcase_add_test(core, socket_connection);
 	tcase_add_test(core, irc_connection);
+
+	suite_add_tcase(suite, sread);
+	tcase_add_checked_fixture(sread, socketpair_open, socketpair_close);
+	tcase_add_test(sread, socket_readbytes);
+	tcase_add_test(sread, socket_readline);
 
 	return suite;
 }
