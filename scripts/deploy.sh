655719c4ed3f1763890b22a38d1035a3306b754a
Thomas Kapoulas <tomkap@kropotkin.home.tomkap.gr>
2015-06-12 02:34:59 +0300

    Double quote to prevent globbing and word splitting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ OPERATION=$1
 CURRENT=bin/irc-bot
 OLD=irc-bot-old
 
-if [ $OPERATION == "-u" ]; then
+if [ "$OPERATION" == "-u" ]; then
 	cp -f $CURRENT $OLD
 	git pull > /dev/null 2>&1 && make -s -j3 release
 	if [ $? -ne 0 ]; then
@@ -16,7 +16,7 @@ if [ $OPERATION == "-u" ]; then
 		echo "Already at the latest version"
 		exit 1
 	fi
-elif [ $OPERATION == "-d" ]; then
+elif [ "$OPERATION" == "-d" ]; then
 	cmp -s $CURRENT $OLD
 	if [ $? -eq 0 ]; then
 		echo "Already at the oldest version"
