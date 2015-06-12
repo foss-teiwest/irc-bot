b7b624977b1a11c55e51f08fae9837de90781b06
Bill Kolokithas <kolokithas.b@gmail.com>
2015-06-12 04:02:32 +0300

    store the current version as well

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
 OPERATION=$1
+NEW=versions/irc-bot-new
+OLD=versions/irc-bot-old
 CURRENT=bin/irc-bot
-OLD=irc-bot-old
 
 if [ "$OPERATION" == "-u" ]; then
-	if [ ! -e $OLD ]; then
+	git fetch > /dev/null 2>&1
+	reslog=$(git log HEAD..origin/master --oneline)
+	if [[ "${reslog}" != "" ]]; then
+		git rebase origin/master > /dev/null 2>&1
 		cp -f $CURRENT $OLD
+		make -s -j3 release
+		cp -f $CURRENT $NEW
+		exit
 	fi
-	git pull > /dev/null 2>&1 && make -s -j3 release
-	if [ $? -ne 0 ]; then
-		echo "Error while updating git or compile error"
-		exit 1
-	fi
-	cmp -s $CURRENT $OLD
-	if [ $? -eq 0 ]; then
-		echo "Already at the latest version"
-		exit 1
-	fi
+
+	cmp -s $CURRENT $NEW && echo "Already at the latest version" && exit 1
+	cp -f $NEW $CURRENT
 elif [ "$OPERATION" == "-d" ]; then
-	cmp -s $CURRENT $OLD
-	if [ $? -eq 0 ]; then
-		echo "Already at the oldest version"
-		exit 1
-	fi
+	cmp -s $OLD $CURRENT && echo "Already at the oldest version" && exit 1
 	cp -f $OLD $CURRENT
 fi
